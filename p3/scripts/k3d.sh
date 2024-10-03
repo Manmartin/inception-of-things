9656f8e9516a83825cbab0ab63855d42d181b26a
Manmartin <manuelmartinlopez02@gmail.com>
2024-10-03 21:58:13 +0200

    p3 almost finished

--- p3/scripts/k3d.sh
+++ p3/scripts/k3d.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Docker (https://docs.docker.com/engine/install/debian/)
 
 sudo apt-get update
@@ -15,23 +16,24 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-
-# kubectl (https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/)
-
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod u+x kubectl
-mv kubectl /usr/bin/
+sudo apt-get install -y curl docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # k3d (https://k3d.io/v5.6.3/#what-is-k3d)
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | TAG=v5.0.0 bash
-
-
-# Create cluster
-k3d cluster create iot
-
-# Create namespaces
+k3d cluster create my-cluster --api-port 6443 -p 8080:80@loadbalancer -p 8888:30036@loadbalancer --agents 2
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod 777 kubectl
+mv kubectl /usr/bin
 kubectl create namespace argocd
 kubectl create namespace dev
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+curl https://raw.githubusercontent.com/argoproj/argo-cd/v2.10.17/manifests/install.yaml | sed 's/\/usr\/local\/bin\/argocd-server/\/usr\/local\/bin\/argocd-server\n        - --insecure/' > install.yaml
+kubectl apply -n argocd -f install.yml # AQUI
+kubectl apply -f ingress.yml -n argocd
+wget -q https://github.com/argoproj/argo-cd/releases/download/v2.12.3/argocd-linux-amd64 -O argocd
+chmod 777 argocd
+mv argocd /usr/bin
+export podname=$(kubectl get pods -n argocd | grep argocd-server | cut -d ' ' -f1)
+echo $podname
+kubectl wait  --timeout=-1s --for=jsonpath='{.status.phase}'=Running pod/$podname -n argocd
+sleep 10s
+argocd admin initial-password -n argocd | head -n1
